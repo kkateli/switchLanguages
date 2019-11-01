bec519d378d0ae77586dace8b225e375e7fbf526
kkateli <kateli24@outlook.com>
2019-11-01 19:41:31 +1300

    Added swap functionality

--- reparc/src/components/switchLanguages/SwitchLanguages.js
+++ reparc/src/components/switchLanguages/SwitchLanguages.js
@@ -5,15 +5,32 @@ import flag2 from "../../assets/images/USFlag.jpg";
 import "./SwitchLanguage.css";
 import swapButton from "../../assets/images/arrows.png";
 class SwitchLanguage extends Component {
-  // state={
-  //     country1:'German'
-  // }
+  state = {
+    ifClicked: false
+  };
+  swapHandler = () => {
+    this.setState({ ifClicked: !this.state.ifClicked });
+  };
   render() {
+    let lang1 = <UnitLanguage country="German" pic={flag1} />;
+
+    let lang2 = <UnitLanguage country="US" pic={flag2} />;
+    if (this.state.ifClicked !== false) {
+      lang2 = <UnitLanguage country="German" pic={flag1} />;
+
+      lang1 = <UnitLanguage country="US" pic={flag2} />;
+    }
+
     return (
       <div>
-        <UnitLanguage country="German" pic={flag1} />
-<img  className="swapButton" src={swapButton} alt="button" />
-        <UnitLanguage country="US" pic={flag2} />
+        {lang1}
+        <img
+          className="swapButton"
+          src={swapButton}
+          alt="button"
+          onClick={this.swapHandler}
+        />
+        {lang2}
       </div>
     );
   }
